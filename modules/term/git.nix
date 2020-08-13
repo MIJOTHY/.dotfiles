112772e42c4f0fdcaf8181c837d348268356b496
James Griffiths <james.griffiths.2222@gmail.com>
2020-08-13 09:11:20 +0100

    term: switch to home-manager git module

--- modules/term/git.nix
+++ modules/term/git.nix
@@ -1,40 +1,40 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   my = {
-    packages = with pkgs; [
-      git
-      gitAndTools.hub
-      gitAndTools.diff-so-fancy
-    ];
-
-    home.home.file."${config.my.work.dir}/.gitconfig".text = ''
-    [user]
-        email = ${config.my.work.email}
-    '';
-
-    home.home.file."${config.my.personal.dir}/.gitconfig".text = ''
-    [user]
-        email = ${config.my.personal.email}
-    '';
-
-    home.xdg.configFile = {
-      "git/ignore".text = "";
-      "git/config".text = ''
-      [user]
-          name = ${config.my.fullname}
-          email = ${config.my.personal.email}
-
-      [pull]
-          ff = only
-
-      [includeIf "gitdir:~/${config.my.personal.dir}/"]
-          path = ~/${config.my.personal.dir}/.gitconfig
-
-      [includeIf "gitdir:~/${config.my.work.dir}/"]
-          path = ~/${config.my.work.dir}/.gitconfig
-
-      [core]
-          editor = vim
-      '';
+    home = {
+      programs = {
+        git = {
+          enable = true;
+          userName = config.my.fullname;
+          userEmail = config.my.personal.email;
+          extraConfig = {
+            pull = {
+              ff = "only";
+            };
+            core = {
+              editor = "vim";
+            };
+          };
+          includes = [
+            {
+              condition = "gitdir:~/${config.my.personal.dir}/";
+              contents = {
+                user = {
+                  email = config.my.personal.email;
+                };
+              };
+            }
+            {
+              condition = "gitdir:~/${config.my.work.dir}/";
+              contents = {
+                user = {
+                  email = config.my.work.email;
+                };
+              };
+            }
+          ];
+        };
+      };
     };
   };
 }
