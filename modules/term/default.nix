2e26f3f9d0fb31c41098506454813f2cf6d0e6b6
James Griffiths <griffithsjr@james-macbook.local>
2020-08-13 09:03:50 +0100

    term: enable fzf

--- modules/term/default.nix
+++ modules/term/default.nix
@@ -4,29 +4,27 @@
     ./ssh.nix
   ];
   my = {
-    # home = {
-    #   home = {
-    # xdg.configFile = {
-    #   "zsh/rc.d/rc.fzf.zsh".source = <config/fzf/rc.zsh>;
-    #   "zsh/rc.d/rc.term.zsh".text = ''
-    #     alias mkdir='mkdir -p'
-    #     alias cat='bat -p'
-    #     alias wget='wget -c'
-    #     alias bc='bc -lq'
-    #     alias rg='rg --hidden'
-    #     alias tree="tree -a -I '.git'"
-    #     alias grep='grep --color=auto'
-    #     alias fgrep='fgrep --color=auto'
-    #     alias egrep='egrep --color=auto'
-    #     alias exa='exa -h --group-directories-first --git'
-    #     alias ls=exa
-    #     alias l='exa -1a'
-    #     alias ll='exa -la'
-    #     alias lt='exa -lm -s modified'
-    #   '';
-    # };
-    #   };
-    # };
+    home = {
+      xdg.configFile = {
+        "zsh/rc.d/rc.fzf.zsh".source = <config/fzf/rc.zsh>;
+        "zsh/rc.d/rc.term.zsh".text = ''
+            alias mkdir='mkdir -p'
+            alias tree="tree -a -I '.git'"
+            alias exa='exa -h --group-directories-first --git'
+            alias ls=exa
+            alias l='exa -1a'
+            alias ll='exa -la'
+            alias lt='exa -lm -s modified'
+          '';
+      };
+
+      programs = {
+        fzf = {
+          enable = true;
+          enableZshIntegration = true;
+        };
+      };
+    };
 
     packages = with pkgs; [
       coreutils
