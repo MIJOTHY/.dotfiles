4fd7724868bacd6f09bca89d8163c05f4efd2992
James Griffiths <james.griffiths.2222@gmail.com>
2021-01-11 18:05:24 +0000

    term: lorri->nix-direnv
    Lorri would sometimes die and at other times silently fail to refresh
    relevant shell.nix files. Since direnv+nix-direnv's `use nix`ness is
    synchronous, at least this would die explicitly. Also, it doesn't die in
    the same situation, so :shrug:

--- modules/term/direnv.nix
+++ modules/term/direnv.nix
@@ -1,7 +1,18 @@
 { pkgs, ... }: {
   my = {
-    packages = [ pkgs.direnv ];
-    home.xdg.configFile."zsh/rc.d/rc.direnv.zsh".text =
-      ''eval "$(direnv hook zsh)"'';
+    packages = with pkgs; [
+      direnv
+      nix-direnv
+    ];
+    home = {
+      programs = {
+        direnv = {
+          enable = true;
+          enableNixDirenvIntegration = true;
+        };
+      };
+      xdg.configFile."zsh/rc.d/rc.direnv.zsh".text =
+        ''eval "$(direnv hook zsh)"'';
+    };
   };
 }
