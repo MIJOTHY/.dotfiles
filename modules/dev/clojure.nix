5d8c7ef1704832845b43a7860355578bb67c3b71
James Griffiths <james.griffiths.2222@gmail.com>
2021-03-11 09:19:42 +0000

    clojure: add lein profile

--- modules/dev/clojure.nix
+++ modules/dev/clojure.nix
@@ -2,6 +2,9 @@
 with lib;
 {
   config = {
+    my.home.home.file = {
+      ".lein/profiles.clj".source = <config/lein/profiles.clj>;
+    };
     my.packages = with pkgs; [
       clojure
       joker
