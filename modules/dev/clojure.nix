c8ccf15ff6157aa757b8eae6c6dbdbda774d6b32
James Griffiths <james.griffiths.2222@gmail.com>
2021-03-14 14:04:45 +0000

    clojure: default to jdk11, bootclasspath

--- modules/dev/clojure.nix
+++ modules/dev/clojure.nix
@@ -2,13 +2,16 @@
 with lib;
 {
   config = {
-    my.home.home.file = {
-      ".lein/profiles.clj".source = <config/lein/profiles.clj>;
+    my = {
+      home.home.file = {
+        ".lein/profiles.clj".source = <config/lein/profiles.clj>;
+      };
+      packages = with pkgs; [
+        clojure
+        joker
+        (leiningen.override { jdk = openjdk11; })
+      ];
+      env.LEIN_USE_BOOTCLASSPATH="no";
     };
-    my.packages = with pkgs; [
-      clojure
-      joker
-      leiningen
-    ];
   };
 }
