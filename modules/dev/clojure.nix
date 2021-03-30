aa6e9188ecd91eada77aebc4f590aa3268e84067
James Griffiths <james.griffiths.2222@gmail.com>
2021-03-30 11:13:21 +0100

    dev: add clj-kondo to clojure
    The installCheck is borked due to jshell trying to synchronise
    (write/read) with a history file that doesn't exist and lies outside its
    permissions (`/tools/JShell`). So I do the responsible thing and disable
    the whole check.

--- modules/dev/clojure.nix
+++ modules/dev/clojure.nix
@@ -9,6 +9,22 @@ with lib;
       packages = with pkgs; [
         clojure
         joker
+        (clj-kondo.override {
+          graalvm11-ce = pkgs.graalvm11-ce.overrideAttrs (_ : {
+              # FIXME:
+              # jshell fails due to not being able to synchronise the history file
+              # ```
+              # > |  Welcome to JShell -- Version 11.0.10
+              # |  For an introduction type: /help intro
+              #
+              # jshell> $1 ==> 2
+              #
+              # jshell> Exception in thread "main" java.lang.IllegalStateException: java.util.prefs.BackingStoreException: Synchronization failed for node '/tool/JShell/'
+              # ```
+              #
+              # https://github.com/NixOS/nixpkgs/blob/194c1f6aae06239a9c2263066223dda28048b652/pkgs/development/compilers/graalvm/community-edition.nix#L216-L218
+              doInstallCheck = false;
+            });})
         (leiningen.override { jdk = openjdk11; })
       ];
       env.LEIN_USE_BOOTCLASSPATH="no";
